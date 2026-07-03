ee334513e9d6da7a8f0794d0fd4ea382a25de128
harshpatel2405 <harshpatel4504@gmail.com>
2026-07-03 18:57:33 +0530

    dom 2(css)

---
+++ DOM/dom2.js
@@ -0,0 +1,16 @@
+let firstH1 = document.getElementById("h1");
+firstH1.innerText = "Netflix";
+firstH1.style.color = "red"
+firstH1.style.backgroundColor = "black"
+firstH1.style.textAlign = "center"
+firstH1.style.padding = "10px"
+
+
+
+
+document.getElementById("h2").innerText = "Alpha";
+document.getElementById("h3").innerText = "Welcome to jungle";
+
+document.getElementById("p1").innerText = "This is movie1";
+document.getElementById("p2").innerText = "This is movie2";
+document.getElementById("p3").innerText = "This is movie3";
